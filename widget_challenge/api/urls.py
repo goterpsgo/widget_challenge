6561f3410c24375b892c01850da3412877a85f49
Joe Tseng <joe+gitlab@afferent.social>
2018-04-20 21:52:23 -0400

    Updated URL path aliases to properly reflect purposes.

--- widget_challenge/api/urls.py
+++ widget_challenge/api/urls.py
@@ -3,12 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
 urlpatterns = {
-    url(r'^finishes/$', FinishCreateView.as_view(), name="create"),
+    url(r'^finishes/$', FinishCreateView.as_view(), name="finish_create"),
     url(r'^finishes/(?P<pk>[0-9]+)/$',
-        FinishDetailsView.as_view(), name="details"),
-    url(r'^sizes/$', SizeCreateView.as_view(), name="create"),
+        FinishDetailsView.as_view(), name="finish_details"),
+    url(r'^sizes/$', SizeCreateView.as_view(), name="size_create"),
     url(r'^sizes/(?P<pk>[0-9]+)/$',
-        SizeDetailsView.as_view(), name="details"),
+        SizeDetailsView.as_view(), name="size_details"),
+    url(r'^widgets/$', WidgetCreateView.as_view(), name="widget_create"),
+    url(r'^widgets/(?P<pk>[0-9]+)/$',
+        WidgetDetailsView.as_view(), name="widget_details"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
