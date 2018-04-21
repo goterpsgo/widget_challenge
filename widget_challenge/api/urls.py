2ea326518316527bd1471b5aef1cf099086c9618
Joe Tseng <joe+gitlab@afferent.social>
2018-04-21 13:45:55 -0400

    Shows what widgets are of a given size or finish.

--- widget_challenge/api/urls.py
+++ widget_challenge/api/urls.py
@@ -6,15 +6,21 @@ urlpatterns = {
     url(r'^finishes/$', FinishCreateView.as_view(), name="finish_create"),
     url(r'^finishes/(?P<pk>[0-9]+)/$',
         FinishDetailsView.as_view(), name="finish_details"),
+
     url(r'^sizes/$', SizeCreateView.as_view(), name="size_create"),
     url(r'^sizes/(?P<pk>[0-9]+)/$',
         SizeDetailsView.as_view(), name="size_details"),
+
     url(r'^widgets/$', WidgetCreateView.as_view(), name="widget_create"),
+    url(r'^finishes/(?P<pk>[0-9]+)/widgets/$', WidgetCreateView.as_view(), name="widget_create"),
+    url(r'^sizes/(?P<pk>[0-9]+)/widgets/$', WidgetCreateView.as_view(), name="widget_create"),
     url(r'^widgets/(?P<pk>[0-9]+)/$',
         WidgetDetailsView.as_view(), name="widget_details"),
+
     url(r'^orders/$', OrderCreateView.as_view(), name="order_create"),
     url(r'^orders/(?P<pk>[0-9]+)/$',
         OrderDetailsView.as_view(), name="order_details"),
+
     url(r'^orderitems/$', OrderItemCreateView.as_view(), name="orderitem_create"),
     url(r'^widgets/(?P<pk>[0-9]+)/orderitems/$', OrderItemCreateView.as_view(), name="orderitem_create"),
     url(r'^orders/(?P<pk>[0-9]+)/orderitems/$', OrderItemCreateView.as_view(), name="orderitem_create"),
