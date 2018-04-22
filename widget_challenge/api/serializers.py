bea5f97007f448b9d9aecea24873b0988b9cc816
Joe Tseng <joe+gitlab@afferent.social>
2018-04-21 20:56:22 -0400

    1. In "fields", explicitly defined column names are replaced with "__all__".
    2. Added class CategorySerializer.

--- widget_challenge/api/serializers.py
+++ widget_challenge/api/serializers.py
@@ -1,43 +1,40 @@
 from rest_framework import serializers
-from .models import Finish, Size, Widget, Order, OrderItem
+from .models import *
 
 """Serializer to map Model instances into JSON format."""
 class FinishSerializer(serializers.ModelSerializer):
-
+    """Meta class to map serializer's fields with the model fields."""
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
         model = Finish
-        fields = ('id', 'name', 'date_created', 'date_modified')
+        fields = '__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
 
 class SizeSerializer(serializers.ModelSerializer):
-
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
         model = Size
-        fields = ('id', 'name', 'date_created', 'date_modified')
+        fields = '__all__'
+        read_only_fields = ('id', 'date_created', 'date_modified')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
 
 class WidgetSerializer(serializers.ModelSerializer):
-
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
         model = Widget
-        fields = ('id', 'name', 'inventory', 'finish', 'size', 'date_created', 'date_modified')
+        fields = '__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
 
 class OrderSerializer(serializers.ModelSerializer):
-
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
         model = Order
-        fields = ('id', 'date_created', 'date_modified')
+        fields = '__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
 
 class OrderItemSerializer(serializers.ModelSerializer):
-
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
         model = OrderItem
-        fields = ('id', 'count', 'order', 'widget', 'date_created', 'date_modified')
+        fields = '__all__'
         read_only_fields = ('id', 'date_created', 'date_modified')
