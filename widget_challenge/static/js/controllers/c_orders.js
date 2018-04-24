d73f031f1d51d74dde31c0489bdfdb86d23571e3
Joe Tseng <joe+gitlab@afferent.social>
2018-04-24 09:37:03 -0400

    Add widget items to new order.

--- widget_challenge/static/js/controllers/c_orders.js
+++ widget_challenge/static/js/controllers/c_orders.js
@@ -4,13 +4,26 @@ angular
     .module('app')
     .controller('orders_controller', _controller);
 
-function _controller($scope, __env, OrdersService) {
-    $scope.orders = [];
+function _controller($scope, WidgetsService, OrdersService, OrderItemsService) {
+    $scope.items = [];
+    $scope.widgets = [];
+    $scope.active_item = {};
 
     _init();
 
     function _init() {
+        get_widgets();
         get_orders();
+    }
+
+    function get_widgets() {
+        WidgetsService
+            .get_widgets()
+            .then(
+                function(widgets) {
+                    $scope.widgets = widgets.results;
+                }
+            );
     }
 
     function get_orders() {
@@ -18,9 +31,60 @@ function _controller($scope, __env, OrdersService) {
             .get_orders()
             .then(
                 function(orders) {
-                    $scope.orders = orders.results;
+                    $scope.items = orders.results;
+                    console.log("[35] ", $scope.items);
                 }
             );
     }
+
+    function refresh_page() {
+        $scope.active_item = {};
+        get_orders();
+    }
+
+    $scope.reset_active_item = function() {
+        $scope.active_item = {};
+    };
+
+    $scope.add_item_to_order = function(item) {
+        $scope.items.push(item);
+        console.log("[51] ", item);
+        console.log("[51] ", $scope.items);
+    };
+
+    $scope.submit_form = function() {
+        if ($scope.active_item.id != null) {
+            OrdersService
+                .update_order($scope.active_item)
+                .then(
+                    function(results) {
+                        refresh_page();
+                    }
+                )
+        }
+        else {
+            OrdersService
+                .add_order($scope.active_item)
+                .then(
+                    function(results) {
+                        refresh_page();
+                    }
+                )
+        }
+    };
+
+    $scope.delete_item = function(item) {
+        OrdersService
+            .delete_order(item.id)
+            .then(
+                function(results) {
+                    refresh_page();
+                }
+            )
+    };
+
+    $scope.edit_entry = function(item) {
+        $scope.active_item = _.clone(item);
+    };
 }
 
