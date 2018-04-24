df7e66064b4b9c478633a69557228fb41dee4dd9
Joe Tseng <joe+gitlab@afferent.social>
2018-04-24 09:07:56 -0400

    Widget controller methods.

--- widget_challenge/static/js/controllers/c_widgets.js
+++ widget_challenge/static/js/controllers/c_widgets.js
@@ -4,13 +4,50 @@ angular
     .module('app')
     .controller('widgets_controller', _controller);
 
-function _controller($scope, __env, WidgetsService) {
-    $scope.widgets = [];
+function _controller($scope, WidgetsService, CategoriesService, FinishesService, SizesService) {
+    $scope.items = [];
+    $scope.categories = [];
+    $scope.finishes = [];
+    $scope.sizes = [];
+    $scope.active_item = {};
 
     _init();
 
     function _init() {
+        get_categories();
+        get_finishes();
+        get_sizes();
         get_widgets();
+    }
+
+    function get_categories() {
+        CategoriesService
+            .get_categories()
+            .then(
+                function(categories) {
+                    $scope.categories = categories.results;
+                }
+            );
+    }
+
+    function get_finishes() {
+        FinishesService
+            .get_finishes()
+            .then(
+                function(finishes) {
+                    $scope.finishes = finishes.results;
+                }
+            );
+    }
+
+    function get_sizes() {
+        SizesService
+            .get_sizes()
+            .then(
+                function(sizes) {
+                    $scope.sizes = sizes.results;
+                }
+            );
     }
 
     function get_widgets() {
@@ -18,9 +55,55 @@ function _controller($scope, __env, WidgetsService) {
             .get_widgets()
             .then(
                 function(widgets) {
-                    $scope.widgets = widgets.results;
+                    $scope.items = widgets.results;
                 }
             );
     }
+
+    function refresh_page() {
+        $scope.active_item = {};
+        get_widgets();
+    }
+
+    $scope.reset_active_item = function() {
+        $scope.active_item = {};
+    };
+
+    $scope.submit_form = function() {
+        if ($scope.active_item.id != null) {
+            WidgetsService
+                .update_widget($scope.active_item)
+                .then(
+                    function(results) {
+                        refresh_page();
+                    }
+                )
+        }
+        else {
+            WidgetsService
+                .add_widget($scope.active_item)
+                .then(
+                    function(results) {
+                        refresh_page();
+                    }
+                )
+        }
+    };
+
+    $scope.delete_item = function(item) {
+        WidgetsService
+            .delete_widget(item.id)
+            .then(
+                function(results) {
+                    refresh_page();
+                }
+            )
+    };
+
+    $scope.edit_entry = function(item) {
+        $scope.active_item = _.clone(item);
+        console.log("[105] ", $scope.active_item);
+        console.log("[106] ", $scope.active_item.category);
+    };
 }
 
